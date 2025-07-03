de6883e88978f86f9e479c848e4c72946204134c
Hemant-kumarr <suhemant685@gmail.com>
2025-07-03 18:05:42 +0530

    form mac

---
+++ linearsearch.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+void binarySearch(int arr[], int n) {
+    int target;
+    int a = 0;
+    printf("Enter the target element you want to search: ");
+    scanf("%d", &target);
+    
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            printf("Element found at index %d\n", i);
+            a++;
+        }
+    }
+    
+    if (a == 0) {
+        printf("Element not found in array\n");
+    } else {
+        printf("Count: %d\n", a);
+    }
+}
+
+int main() {
+    int arr[50];
+    int n;
+    printf("Enter the number of elements you want in array: ");
+    scanf("%d", &n);
+    
+    for (int i = 0; i < n; i++) {
+        printf("Enter the element number %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    
+    binarySearch(arr, n);
+
+    return 0;
+}
